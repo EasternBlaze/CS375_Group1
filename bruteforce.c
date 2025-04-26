1646c8c205914f8e8b47864cb10c8e70ed738f89
bdang12 <billbinh04@gmail.com>
2025-04-26 11:47:57 -0400

    simple brute force implement
    simple brute force implement with time complexity with O(n) best and O(mn) for worst case.

---
+++ bruteforce.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+//Using bruteforce func
+void bruteforce(char* pattern, char *text)
+{
+    int M = strlen(pattern);
+    int N = strlen(text);
+
+    for(int i =0 ; i<=N-M; i++)
+    {
+        int j;
+        //check pattern match starting from pos i
+        for(j=0; j<M; j++)
+        {
+            if(text[i+j] != pattern[j])
+            {
+                break;
+            }
+        }
+        //If pattern is match
+        if(j==M) {
+            printf("Pattern found at index %d\n", i);
+
+        }
+    }
+}
+
+int main()
+{
+    char text[] =  "ABABDABACDABABCABAB";
+    char pattern[]= "ABABCABAB";
+    bruteforce(pattern, text);
+}
